b581d3aa04948373e20d9913e40c7ab7b57cd4c7
Daniel <1602268076@qq.com>
2018-07-06 11:03:42 +0800

    上传babel-node

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import mod from './lib';
+console.log(mod.counter); // 3
+mod.incCounter();
+console.log(mod.counter); // 4
---
+++ src/lib.js
@@ -0,0 +1,8 @@
+var counter = 3;
+function incCounter() {
+  counter++;
+}
+export default {
+  counter: counter,
+  incCounter: incCounter,
+};
